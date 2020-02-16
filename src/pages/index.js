c65ef48304bec379259d64c1277125da1598b0af
spiffy <pravien.mangal@gmail.com>
2020-02-16 21:58:53 +0000

    Style setup, font setup and banner styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo/seo"
+import Banner from "../components/Banner/Banner";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Banner />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
