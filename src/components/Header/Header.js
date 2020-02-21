0bdb8b2258faa73f575a8339a1d8f20dc23f674b
spiffy <pravien.mangal@gmail.com>
2020-02-21 01:28:42 +0000

    Navigation for desktop added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,28 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Nav from '../Nav/Nav'
 
-const Header = ({ siteTitle }) => (
-  <header className="header">
-    <div className="header-content container">
-      <h1 style={{ margin: 0 }}>
+const Header = ({ siteTitle }) => {
+
+  const navOptions = [
+    { title: "About Us", url: "/page-2/" },
+    { title: "Our Courses", url: "/page-2/" },
+    { title: "Gallery", url: "/page-2/" },
+    { title: "Contact Us", url: "/page-2/" }
+  ];
+
+  return (
+    <header className="header">
+      <div className="header-container container">
         <Link to="/" className="logo">
           {siteTitle}
         </Link>
-      </h1>
-    </div>
-  </header>
-)
+        <Nav options={navOptions} />
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
