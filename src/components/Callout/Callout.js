646563428af1bea5c7bf8d9432b992c01ea64bd8
spiffy <pravien.mangal@gmail.com>
2020-02-25 22:19:11 +0000

    Callout component refactored

--- src/components/Callout/Callout.js
+++ src/components/Callout/Callout.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import cprImage from '../../images/cpr-2.jpg'
+import PropType from 'prop-types'
 
-const Callout = () => {
+const Callout = ({ title, link, image }) => {
   return (
     <div class="container">
       <div class="grid flex">
         <div class="medium-span-1-2 large-span-1-3 transition-start">
-          <a class="callout-list" href="#">
+          <a class="callout-list" href={link}>
             <div class="box shadow-box-full callout-box lazy-image">
               <div className="lazy-load" style={{
-                backgroundImage: `url('${cprImage}')`
+                backgroundImage: `url('${image}')`
               }}></div>
               <div class="box-content vertical-top">
-                <h2>School to School Support</h2>
+                <h2>{title}</h2>
                 <hr />
               </div>
             </div>
@@ -21,6 +21,12 @@ const Callout = () => {
       </div>
     </div>
   );
+};
+
+Callout.prototypes = {
+  title: PropType.string.isRequired,
+  link: PropType.string.isRequired,
+  image: PropType.string.isRequired
 };
 
 export default Callout;
