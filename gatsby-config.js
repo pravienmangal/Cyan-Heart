6cb0da0e51dd915ddda86fe2469403ca80a73d8b
spiffy <pravien.mangal@gmail.com>
2020-03-08 21:09:45 +0000

    gatsby-transformer-json added to plugons

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
