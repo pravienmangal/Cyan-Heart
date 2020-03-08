00600e927f75b8b614337a50a63cd4189a89277c
spiffy <pravien.mangal@gmail.com>
2020-03-08 21:50:49 +0000

    Carousel styles and script move to styles folder

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Slider from 'react-slick'
 import PropType from 'prop-types'
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "../../styles/slick"
+import "../../styles/slick.css"
+import "../../styles/slick-theme.css"
+
+// import "slick-carousel/slick/slick.css"
+// import "slick-carousel/slick/slick-theme.css"
 
 const Carousel = ({ children }) => {
   const settings = {
