38bcf67a2edcb2cc3e076b259b23f3c0e5aa5249
spiffy <pravien.mangal@gmail.com>
2020-04-12 02:19:26 +0100

    Temporaray isUserConfirmed logic added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -33,18 +33,9 @@ const Layout = ({ children }) => {
   console.log('identity', identity)
   const isLoggedIn = identity && identity.isLoggedIn
 
-  // useEffect(() => {
-  //   if (identity) {
-  //     window.netlifyIdentity.on("init", user => {
-  //       if (!user) {
-  //         window.netlifyIdentity.on("login", () => {
-  //           document.location.href = "/admin/";
-  //         });
-  //       }
-  //     });
-  //   }
-  //   console.log('Window identity', window.netlifyIdentity);
-  // }, []);
+  useEffect(() => {
+    setDialog(identity.isConfirmedUser)
+  }, []);
 
   console.log('identity', identity);
   console.log('name', name);
